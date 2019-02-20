6fda6f28a237394693a3ac10780246a55d993ada
Simon Sun <bardrabbit@gmail.com>
2019-02-20 14:08:47 -0500

    update budgetController percentage

--- budget app/app.js
+++ budget app/app.js
@@ -4,6 +4,20 @@ var budgetController = (function() {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        }
+        
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     }
 
     var Income = function(id, description, value) {
@@ -91,6 +105,19 @@ var budgetController = (function() {
                 data.percentage = -1;
             }
             
+        },
+
+        calculatePercentages: function() {
+            data.allItems.exp.forEach(function(cur) {
+                cur.calcPercentage(data.totals.inc);
+            })
+        },
+
+        getPercentages: function() {
+            var allPerc = data.allItems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPerc;
         },
 
         getBudget: function() {
@@ -230,11 +257,13 @@ var controller = (function(budgetCtrl, UIctrl){
 
     var updatePercentages = function() {
         // 1. Calculate percentages
-
+        budgetCtrl.calculatePercentages();
 
         // 2. read percentages from the budgetController
+        var percentages = budgetCtrl.getPercentages();
 
         // 3. Update the percentages
+        console.log(percentages);
     }
    
     var ctrlAddItem = function() {
